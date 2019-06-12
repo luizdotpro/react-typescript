7a90b76a745fa7582db4b128c743d9ee538e7c30
Luiz Carneiro <luiz.carneiro@sport1.de>
2019-06-12 15:08:11 +0200

    Added react.suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,78 @@
-import React from 'react';
-import { Store } from './Store';
+import React from 'react'
+import { Store } from './Store'
 import './index.css'
 import { IAction, IEpisode } from './interfaces'
 
+const EpisodesList = React.lazy<any>(() => import('./EpisodesList'))
 
 const App: React.FunctionComponent = (): JSX.Element => {
+    const { state, dispatch } = React.useContext(Store)
 
-  const { state, dispatch } = React.useContext(Store)
-
-  React.useEffect(() => {
-    state.episodes.length === 0 && fetchDataAction()
-    console.log('useEffect hook')
-  })
-
-  const fetchDataAction = async () => {
-    const data = await fetch('https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes')
-    const dataJSON = await data.json()
-    return dispatch({
-      type: 'FETCH_DATA',
-      payload: dataJSON._embedded.episodes
+    React.useEffect(() => {
+        state.episodes.length === 0 && fetchDataAction()
+        console.log('useEffect hook')
     })
-  }
 
-  const toggleFavAction = (episode: IEpisode): IAction => {
-    const episodeInFav: boolean = state.favourites.includes(episode)
-    console.log(episodeInFav)
-
-    let dispatchObj = {
-      type: 'ADD_FAV',
-      payload: episode
-    }
-
-    if (episodeInFav){
-      const favWithoutEpisode = state.favouritesv.filter( 
-        (fav: IEpisode)  => fav.id !== episode.id
+    const fetchDataAction = async () => {
+        const data = await fetch(
+            'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
         )
-
-      dispatchObj = {
-        type: 'REMOVE_FAV',
-        payload: favWithoutEpisode
-      } 
+        const dataJSON = await data.json()
+        return dispatch({
+            type: 'FETCH_DATA',
+            payload: dataJSON._embedded.episodes,
+        })
     }
 
-    return dispatch(dispatchObj)
-  }
+    const toggleFavAction = (episode: IEpisode): IAction => {
+        const episodeInFav: boolean = state.favourites.includes(episode)
+        console.log(episodeInFav)
 
-  console.log('state -', state)
+        let dispatchObj = {
+            type: 'ADD_FAV',
+            payload: episode,
+        }
 
-  return (
-    <>
-      <header className="header">
-        <div>
-          <h2>Rick and Morty Episode picker.</h2>
-          <p> Pick your favourite episode!</p>
-        </div>
-        <div>
-          <p> {`Number of favourites: ${state.favourites.length}`}</p>
-        </div>
-      </header>
-      <section className="episodeLayout">
-        {state.episodes.map((episode: IEpisode) => {
-          if (episode.image) {
-            return (
-              <section
-                key={episode.id}
-                className="episodeBox"
-              >
-                <img src={episode.image.medium} alt={`Rick and Morty ${episode.name}`} />
-                <div>{episode.name}</div>
-                <section>Season: {episode.season} Number: {episode.number} </section>
-                <button 
-                    type="button" 
-                    onClick={() => toggleFavAction(episode)}>
-                      {state.favourites.includes(episode) ? 'Unfavourite' : 'Favourite'}
-                </button>
-              </section>
+        if (episodeInFav) {
+            const favWithoutEpisode = state.favouritesv.filter(
+                (fav: IEpisode) => fav.id !== episode.id
             )
-          }
-          else {
-            return null
-          }
-        })}
-      </section>
-    </>
-  );
+
+            dispatchObj = {
+                type: 'REMOVE_FAV',
+                payload: favWithoutEpisode,
+            }
+        }
+
+        return dispatch(dispatchObj)
+    }
+
+    const props = {
+        episodes: state.episodes,
+        toggleFavAction,
+        favourites: state.favourites,
+    }
+
+    console.log('state -', state)
+
+    return (
+        <>
+            <header className="header">
+                <div>
+                    <h2>Rick and Morty Episode picker.</h2>
+                    <p> Pick your favourite episode!</p>
+                </div>
+                <div>
+                    <p> {`Number of favourites: ${state.favourites.length}`}</p>
+                </div>
+            </header>
+            <React.Suspense fallback={<div> Loadding ... </div>}>
+                <section className="episodeLayout">
+                    <EpisodesList {...props} />
+                </section>
+            </React.Suspense>
+        </>
+    )
 }
 
-export default React.memo(App);
+export default React.memo(App)
---
+++ src/EpisodesList.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { IEpisode } from './interfaces'
+
+export default function EpisodesList(props: any): Array<JSX.Element> {
+    const { episodes, toggleFavAction, favourites } = props
+    return episodes.map((episode: IEpisode) => {
+        if (episode.image) {
+            return (
+                <section key={episode.id} className="episodeBox">
+                    <img src={episode.image.medium} alt={`Rick and Morty ${episode.name}`} />
+                    <div>{episode.name}</div>
+                    <section style={{ display: 'flex', justifyContent: 'space-between'}}>
+                        Season: {episode.season} Number: {episode.number}
+                    </section>
+                    <button type="button" onClick={() => toggleFavAction(episode)}>
+                        {favourites.includes(episode) ? 'Unfavourite' : 'Favourite'}
+                    </button>
+                </section>
+            )
+        } else {
+            return null
+        }
+    })
+}

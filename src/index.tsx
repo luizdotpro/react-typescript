784b4590816ab00300bb9b8bf97994188e0bda3c
Luiz Carneiro <luuizpaulo@gmail.com>
2020-06-08 23:36:38 +0200

    default on routerPage

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,12 @@ import { Router, RouteComponentProps } from '@reach/router'
 import HomePage from "./HomePage"
 import FavPage from "./FavPage"
 
-const RouterPage = (props: {pageComponent: JSX.Element} & RouteComponentProps) => props.pageComponent
+const RouterPage = (props: { pageComponent: JSX.Element } & RouteComponentProps) => props.pageComponent
 
 ReactDOM.render(<StoreProvider>
     <Router>
         <App path='/' >
-            <RouterPage pageComponent={<HomePage />} path="/" />
+            <RouterPage default pageComponent={<HomePage />} path="/" />
             <RouterPage pageComponent={<FavPage />} path="/faves" />
         </App>
     </Router>

df505a81cd9efbaf9ffbabafe6bbed12df186694
Luiz Carneiro <luuizpaulo@gmail.com>
2020-06-09 00:06:36 +0200

    Update readme

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,8 @@ const RouterPage = (props: { pageComponent: JSX.Element } & RouteComponentProps)
 
 ReactDOM.render(<StoreProvider>
     <Router>
-        <App path='' >
-            <RouterPage default pageComponent={<HomePage />} path="" />
+        <App path='/' >
+            <RouterPage pageComponent={<HomePage />} path="/" />
             <RouterPage pageComponent={<FavPage />} path="/faves" />
         </App>
     </Router>

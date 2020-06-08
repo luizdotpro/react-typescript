3a36422fec6be65a5e4432471cb9adcc588b0c2a
Luiz Carneiro <luuizpaulo@gmail.com>
2020-06-08 23:24:16 +0200

    Updates

---
+++ precache-manifest.6fd2dc62e3834063122eb7f16b1c4194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e605ba9f18c38ebb9360bd8a7c639566",
+    "url": "/react-typescript/index.html"
+  },
+  {
+    "revision": "248da04defca14e2b541",
+    "url": "/react-typescript/static/css/main.ca006aa8.chunk.css"
+  },
+  {
+    "revision": "106d79d75080104fd35a",
+    "url": "/react-typescript/static/js/0.82368298.chunk.js"
+  },
+  {
+    "revision": "8f5ca202c7345181e523",
+    "url": "/react-typescript/static/js/3.16d5c38f.chunk.js"
+  },
+  {
+    "revision": "248da04defca14e2b541",
+    "url": "/react-typescript/static/js/main.8376d0a5.chunk.js"
+  },
+  {
+    "revision": "f4a35407526a6d4b81ad",
+    "url": "/react-typescript/static/js/runtime~main.6dbbc307.js"
+  }
+]);

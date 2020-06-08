ff798d1a6f7f940ef832b31b7578ceaaa87be20f
Luiz Carneiro <luuizpaulo@gmail.com>
2020-06-09 00:32:10 +0200

    Moving all the files to root

---
+++ precache-manifest.a19547a2c31db2d71b2867d617feb4f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5f4edef2753d6f9a0230c87d71541e",
+    "url": "/react-typescript/index.html"
+  },
+  {
+    "revision": "d13f8295a9a30b34b553",
+    "url": "/react-typescript/static/css/main.ca006aa8.chunk.css"
+  },
+  {
+    "revision": "106d79d75080104fd35a",
+    "url": "/react-typescript/static/js/0.82368298.chunk.js"
+  },
+  {
+    "revision": "8f5ca202c7345181e523",
+    "url": "/react-typescript/static/js/3.16d5c38f.chunk.js"
+  },
+  {
+    "revision": "d13f8295a9a30b34b553",
+    "url": "/react-typescript/static/js/main.ee827e36.chunk.js"
+  },
+  {
+    "revision": "f4a35407526a6d4b81ad",
+    "url": "/react-typescript/static/js/runtime~main.6dbbc307.js"
+  }
+]);
